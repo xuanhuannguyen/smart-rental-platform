e3265f68e3fbfda62bec7a5ef4e2534192a816e2
Hoangminh06 <minhnh0806@gmail.com>
2026-05-26 08:18:04 +0700

    add application sevices registration

--- server/src/SmartRentalPlatform.Application/ApplicationServiceRegistration.cs
+++ server/src/SmartRentalPlatform.Application/ApplicationServiceRegistration.cs
@@ -3,6 +3,10 @@ using SmartRentalPlatform.Application.Auth;
 using SmartRentalPlatform.Application.Users;
 using SmartRentalPlatform.Application.Profiles;
 using SmartRentalPlatform.Application.Roles;
+using SmartRentalPlatform.Application.Administrative;
+using SmartRentalPlatform.Application.Amenities;
+using SmartRentalPlatform.Application.RoomingHouses;
+using SmartRentalPlatform.Application.Rooms;
 
 namespace SmartRentalPlatform.Application;
 
@@ -14,6 +18,10 @@ public static class ApplicationServiceRegistration
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IProfileService, ProfileService>();
         services.AddScoped<IRoleService, RoleService>();
+        services.AddScoped<IAdministrativeService, AdministrativeService>();
+        services.AddScoped<IAmenityService, AmenityService>();
+        services.AddScoped<IRoomingHouseService, RoomingHouseService>();
+        services.AddScoped<IRoomService, RoomService>();
         return services;
     }
 }
