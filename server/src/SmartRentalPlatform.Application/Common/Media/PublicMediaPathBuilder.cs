993c8d1e5ca1297f261c6eb390fa33bb7537033e
Nguyễn Xuân Huân <nguyenxuanhuan.dev@gmail.com>
2026-07-22 19:14:29 +0700

    Bust public media cache

--- server/src/SmartRentalPlatform.Application/Common/Media/PublicMediaPathBuilder.cs
+++ server/src/SmartRentalPlatform.Application/Common/Media/PublicMediaPathBuilder.cs
@@ -2,8 +2,10 @@ namespace SmartRentalPlatform.Application.Common.Media;
 
 public static class PublicMediaPathBuilder
 {
+    private const string PublicMediaVersion = "real-media-20260722";
+
     public static string Build(Guid mediaAssetId)
     {
-        return $"/api/media/public/{mediaAssetId:D}";
+        return $"/api/media/public/{mediaAssetId:D}?v={PublicMediaVersion}";
     }
 }
